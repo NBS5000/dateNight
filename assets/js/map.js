55bbbfa42663bb337871d5c39291b72d9ec561ee
NBS5000 <steve.nbs.barry@gmail.com>
2022-01-17 22:05:21 +1100

    weather loop works and map functions working correctly, trying to call weather api now

--- assets/js/map.js
+++ assets/js/map.js
@@ -113,7 +113,7 @@ function callTom(url){
                 setTimeout(function(){},1000);
             }
             
-    route(myLat,myLon,$lat,$lon);
+            route(myLat,myLon,$lat,$lon);
 
             // route(myLat,myLon,$lat,$lon);
             // if(!jDist || !jTime){
@@ -149,8 +149,7 @@ function route(alat, alon, blat, blon){
         console.log(jDist);
         console.log(jTime);
         /*redirects to date screen*/
-        //document.location.href(dateUrl);
-        document.location.href = dateUrl;
+        // document.location.href = dateUrl;
         /* call's Mona's function*/
         getCityWeather($lat,$lon,dateDate);
         // document.getElementById("myMap").innerHTML = "<iframe width='100%' height='100%' frameborder='0' scrolling='no' marginheight='0'marginwidth='0'src='https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q="+$lat+","+$lon+"&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed'></iframe>";
--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,26 +2,16 @@ var cityData;
 var cityWeather;
 // var theDate;
 
-function getCityWeatherFromName(name){
-    //construct request URL to get city coordinates from name
-    var requestCityCoordinates = "http://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=022816ce4f8542d4f9f3d06e40efbb54";
-    //call the server API to get city coordinates from name
-    fetch(requestCityCoordinates)
-    .then(function (response) {
-    return response.json();
-    })
-    .then(function (data) {
-    cityData = data;
-    //get the weather data using coordinates obtained
-    getCityWeather(cityData.coord.lat, cityData.coord.lon);
-    });
-}
+
 // debugger;
 // theDate = "15/1/2022";
 // getCityWeather("-33.44618","151.44375",theDate);
 function getCityWeather(lat, lon, theDate){
+    document.location.href = dateUrl;
+    console.log(lat + " - " + lon + " -" + theDate);
     // debugger;
     //construct request URL to get city weather info from coordinates
+
     var requestCityWeather = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=metric&appid=022816ce4f8542d4f9f3d06e40efbb54";    
     fetch(requestCityWeather)
     .then(function (response){
@@ -50,27 +40,35 @@ function displayWeather(date){
     // console.log(cityWeather.daily);
     var i;
     for(i=0;i<8;i++){
+
         //search for the required day within the data returned by openweather API
         // if(new Date(cityWeather.daily[i].dt*1000).toString()==Date(date.toString()){
         // var checkDate = new Date(cityWeather.daily[i].dt).getDay() + "/" + new Date(cityWeather.daily[i].dt).getMonth() + "/" + new Date(cityWeather.daily[i].dt).getFullYear();
-        var day = new Date(cityWeather.daily[i].dt*1000);
-        var dayM = moment(day).format("D/M/YYYY");
+        // var day = new Date(cityWeather.daily[i].dt*1000);
+        // var resDate = new Date(cityWeather.daily[i].dt);
 
-        if(dayM == date){
+        // var checkDate = resDate.getFullYear() + "-" + resDate.getMonth() + "-" + resDate.getDay();
+        var checkDate = moment.unix(cityWeather.daily[i].dt).format("YYYY-MM-DD");
+        // var dayM = moment(day).format("D/M/YYYY");
+        // var checkDate = moment(resDate, "DD-MM-YYYY");
+        console.log(date);
+        console.log(checkDate);
+        // console.log(resDate);
+        if(checkDate == date){
             
             //stop looping because we found the required day
             break;
         }
     }
     //display data for cityWeather.daily[i]-->the date of dateNight
-    // console.log(i);
+    console.log(i);
     var icon = cityWeather.daily[i].weather[0].icon;
     var iconLink = "http://openweathermap.org/img/w/" + icon + ".png";
     // console.log(iconLink);
 
-    document.getElementById("temp").textContent = "Temp: " + cityWeather.daily[i].temp.day + "c";
+    document.getElementById("temp").innerHTML = "Temp: " + cityWeather.daily[i].temp.day + "&deg;c";
     //document.getElementById("humidity").textContent = "Humidity: " + cityWeather.daily[i].humidity + "%";
-    document.getElementById("wind").textContent = "Wind: " + cityWeather.daily[i].wind_speed * 3.6 + "kph";
+    document.getElementById("wind").innerHTML = "Wind: " + cityWeather.daily[i].wind_speed * 3.6 + "kph";
     
     document.getElementById("weatherIcon").src = iconLink;
 }
