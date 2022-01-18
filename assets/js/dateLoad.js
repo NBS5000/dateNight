7f1989196bd9d8533c0d9677141bde459134a7c6
NBS5000 <steve.nbs.barry@gmail.com>
2022-01-18 22:30:07 +1100

    css relating to clothes widget moved to clothes.css file from main, same with modal css items, date load js updated to run drinks functions, drinks.css created to handle drinks widget, weather html amended by cutting onload and adding to date.html, drinks html amended for design of widget, empty and old colde removed from mpa.js

--- assets/js/dateLoad.js
+++ assets/js/dateLoad.js
@@ -8,7 +8,7 @@ function dateLoad(){
 
     var uLat, uLon, uDate, uDist, uTime, urlSearch, urlPar;
 
-
+    debugger;
     urlSearch = window.location.search;
     urlPar = new URLSearchParams(urlSearch);
     uLat = urlPar.get("lat");
@@ -16,7 +16,11 @@ function dateLoad(){
     uDate = urlPar.get("date");
     uDist = urlPar.get("dist");
     uTime = urlPar.get("time");
+    
+    getCocktail();
+    getBeer();
 
     getCityWeather(uLat,uLon,uDate);
     dateToDateDisplay(uDate);
+
 }
